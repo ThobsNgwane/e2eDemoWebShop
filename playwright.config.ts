e236dd47af5c70d43a02b2a9e76ca8f6f98b6dd7
Thobani Ngwane <NgwanaseADO@gmail.com>
2025-07-25 08:53:28 +0200

    Testing commit

--- playwright.config.ts
+++ playwright.config.ts
@@ -29,6 +29,12 @@ export default defineConfig({
     // baseURL: 'http://localhost:3000',
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+    launchOptions: {
+      args: ['--start-maximized']},
+  },
+
+  use:{
+    
   },
 
  
